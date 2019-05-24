28feb5ced67af5eefe791de026320ef63e69fd9d
elihorovitz <eli.horovitz@mail.huji.ac.il>
2019-05-24 12:08:39 +0300

    small demo app

---
+++ app/src/main/java/com/example/navigation_app/Main3Activity.java
@@ -0,0 +1,66 @@
+package com.example.navigation_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
+
+public class Main3Activity extends AppCompatActivity {
+
+    private static final Pattern sPattern
+            = Pattern.compile("^[0-9]{5}$");
+
+    EditText txt;
+
+    private boolean isValid(CharSequence s) {
+        return sPattern.matcher(s).matches();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        txt = (EditText) findViewById(R.id.digitEditor);
+        txt.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (isValid(txt.getText()))
+                {
+                  end();
+                }
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // TODO Auto-generated method stub
+
+            }
+
+
+        });
+    }
+
+    public void end()
+    {
+        Intent first = new Intent(this, MainActivity.class);
+        first.putExtra(getString(R.string.digits), txt.getText().toString());
+        startActivity(first);
+        finish();
+    }
+
+
+
+}
--- app/src/main/java/com/example/navigation_app/MainActivity.java
+++ app/src/main/java/com/example/navigation_app/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.navigation_app;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
+    private final int GO_THIRD = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String currText;
+        if (getIntent().getStringExtra(getString(R.string.digits)) != null)
+        {
+            currText = getIntent().getStringExtra(getString(R.string.digits));
+            TextView textView = findViewById(R.id.forDigits);
+            textView.setText(String.format("you have entered: %s", currText));
+        }
     }
+
+    public void goSecond(View view) {
+        Intent second = new Intent(this, Main2Activity.class);
+        startActivity(second);
+        finish();
+    }
+
+    public void goThird(View view) {
+        Intent third = new Intent(this, Main3Activity.class);
+        startActivity(third);
+        finish();
+
+    }
+
+
 }
+
